cd7d4b286312eef1f16ce0385dd05764603b4d25
Jaroslav Beran <jaroslav.beran@gmail.com>
2018-08-11 11:31:22 +0200

    crtgom.sh

--- src/crtgom.sh
+++ src/crtgom.sh
@@ -3,9 +3,10 @@
 outfile=qcsrc.gom2
 
 echo "#define _AS400_" > $outfile
-sed '/#include "pisk_lib.h"/d' ./pisk_lib.h | fold -s -w80 >> $outfile
-sed '/#include "pisk_lib.h"/d' ./pisk_lib.c | fold -s -w80 >> $outfile
-sed '/#include "pisk_lib.h"/d' ./main_con.c | fold -s -w80 >> $outfile
+echo " " >> $outfile
 
+sed '/#include "pisk_lib.h"/d' pisk_lib.h | indent -l75 >> $outfile
+sed '/#include "pisk_lib.h"/d' pisk_lib.c | indent -l75 >> $outfile
+sed '/#include "pisk_lib.h"/d' main_con.c | indent -l75 >> $outfile
 
 echo "$outfile created!"
