a1c5fdb7d1f992bd495d7a9e363213f9f596f5da
Jaroslav Beran <jaroslav.beran@gmail.com>
2018-08-13 00:09:57 +0200

    AS400

--- src/crtgom.sh
+++ src/crtgom.sh
@@ -6,12 +6,12 @@
 outfile=qcsrc.gom
 
 echo "#define _AS400_" > $outfile
-echo "#define _EINSTEIN_" > $outfile
+echo "#define _EINSTEIN_" >> $outfile
 echo " " >> $outfile
 
-sed '/#include "pisk_lib.h"/d' res_con.h  | sed 's/\r//' | indent -st -l75 >> $outfile
-sed '/#include "pisk_lib.h"/d' pisk_lib.h | sed 's/\r//' | indent -st -l75 >> $outfile
+sed 's/\r//' res_con.h | indent -st -l75 >> $outfile
+sed 's/\r//' pisk_lib.h | indent -st -l75 >> $outfile
 sed '/#include "pisk_lib.h"/d' pisk_lib.c | sed 's/\r//' | indent -st -l75 >> $outfile
-sed '/#include "pisk_lib.h"/d' main_con.c | sed 's/\r//' | indent -st -l75 >> $outfile
+sed '/#include "pisk_lib.h"/d' main_con.c | sed '/#include "res_con.h"/d' | sed 's/\r//' | indent -st -l75 >> $outfile
 
 echo "$outfile created!"
